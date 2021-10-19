5db589a5f1d3e58e86b4b261c63e6c84c99cdc18
Kurtis Scheufen <kscheufe@uwo.ca>
2021-10-19 00:08:16 -0400

    Finished lab assignment 5

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the gam
 //initialize the game
 
 // use the value stored in the nextPlayer variable to indicate who the next player is
-
+document.getElementById("next-lbl").innerText = ""+nextPlayer;
 
 //This call will create the buttons needed for the gameboard.
 createGameBoard()
@@ -11,7 +11,12 @@ createGameBoard()
 function createGameBoard()
 {
     // Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
-   
+   let table = document.getElementById("gameboard");
+    let cells = table.getElementsByTagName('td');
+    for (let i = 0;i<9;i++)
+   {
+       cells[i].innerHTML = '<button>[]</button>';
+   }
 }
 
 // Programatically add 'takeCell' as an event listener to all the buttons on the board
@@ -27,6 +32,13 @@ function takeCell(event)
     /*
         When the button is clicked, the space inside its square brackets is replaced by the value in the nextPlayer before switching it
     */
+   event.target.innerText = ""+nextPlayer;
+   event.target.disabled = true;
+   if (nextPlayer == 'X')
+   nextPlayer = 'O';
+   else 
+   nextPlayer = 'X';
+   document.getElementById("next-lbl").innerText = ""+nextPlayer;
 
     // Make sure the button is clickable only once (I didn't mention how to do that, look it up :) )
 
@@ -34,6 +46,7 @@ function takeCell(event)
     if (isGameOver())
     {
         // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
+        document.getElementById("game-over-lbl").innerText = "game over";
     }
 
     // I'll leave declaring the winner for your intrinsic motivation, it's not required for this assignment 
@@ -42,5 +55,12 @@ function takeCell(event)
 function isGameOver()
 {
     // This function returns true if all the buttons are disabled and false otherwise 
-   
+    for (let i=0; i<btns.length; i++)
+    {
+        if (btns[i].innerText == "[]")
+        {
+            return false;
+        }
+    }
+    return true;
 }
